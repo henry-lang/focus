3f7b69a9e8b0a0d29b766c24ee13e35b1e7d095b
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2021-11-06 19:21:18 -0400

    parse time strings, and fix some weird bugs

--- ext/background.js
+++ ext/background.js
@@ -1,40 +1,48 @@
 importScripts('popup/assets/js/getFromStorage.js')
+let lastTabId = null
 
 //noctural denotes the fact of the start and end times being the start and end times of rest, not work. This combats problems due to working through midnight
+
+function parseTime(time) { // must be formatted in HH:MM
+    let splitTime = time.split(':')
+    let hours = parseInt(splitTime[0]) 
+    let minutes = parseInt(splitTime[1])
+    return hours * 60 + minutes
+}
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     let date = new Date()
     let day = date.getDay()
     let minutesSinceMidnight = date.getHours() * 60 + date.getMinutes()
-    hour - GamepadEvent.getHour()
-    //parse time from start and end
     let blocked = (await get('timings'))[day]
+    let start = parseTime(blocked.start)
+    let end = parseTime(blocked.end)
     let fullurl = new URL(tab.url)
     let domain = fullurl.hostname
-    console.log(domain)
-    //replace hour with minutesSinceMidnight
-    if ((
-        (blocked.start <= hour < blocked.end) && blocked.nocturnal == false) || 
-        (!(blocked.start <= hour < blocked.end) && blocked.nocturnal == true)) {
-            if (blocked.blacklisted.includes(domain)) {
-                for (let n = 0; n < blocked.whitelisted.length; n++) {
-                    console.log(blocked.whitelisted[n])
-                    if (tab.url.startsWith(blocked.whitelisted[n])) {
-                        break;
-                    }
-                    if (n == (blocked.whitelisted.length - 1)) {
-                        chrome.tabs.remove(tabId)
-                        chrome.tabs.create({url: chrome.runtime.getURL("popup/home.html")})
+
+    if (lastTabId != tabId) {
+        if ((
+            (start <= minutesSinceMidnight < end) && blocked.nocturnal == false) || 
+            (!(start <= minutesSinceMidnight < end) && blocked.nocturnal == true)) {
+                if (blocked.blacklisted.includes(domain)) {
+                    for (let n = 0; n < blocked.whitelisted.length; n++) {
+                        if (tab.url.startsWith(blocked.whitelisted[n])) {
+                            break;
+                        }
+                        if (n == (blocked.whitelisted.length - 1)) {
+                            console.log(tabId)
+                            chrome.tabs.remove(tabId)
+                            chrome.tabs.create({url: chrome.runtime.getURL("popup/home.html")})
+                            lastTabId = tabId
+                        }
                     }
                 }
-            }
-    }
-})
+        }
+}})
 
 chrome.runtime.onConnect.addListener((connection) => {
     connection.onMessage.addListener(async (msg) => {
         try {
-            console.log(`Attempting to insert ${msg.page} into blacklist`)
             let day = new Date().getDay()
             data = await get('timings')
             data[day].blacklisted.push(msg.page)
@@ -48,9 +56,15 @@ chrome.runtime.onConnect.addListener((connection) => {
 
 //fix this
 chrome.runtime.onInstalled.addListener((details) => {
-    console.log
-        for (let n = 0; n<7; n++) {
-            chrome.storage.sync.set({timings: {6: {nocturnal: false, start: 09, end: 20, blacklisted: ['www.youtube.com', 'dhkbloecdcojhfkhhmcecnknaogiiagk'], whitelisted: ['https://www.youtube.com/watch?v=LqA35eLEbug']}}})
-        }
+    let defaultConfig = {nocturnal: false, start: '09:50', end: '20:30', blacklisted: ['www.youtube.com'], whitelisted: ['https://www.youtube.com/watch?v=LqA35eLEbug']}
+    chrome.storage.sync.set({timings: {
+        0: defaultConfig,
+        1: defaultConfig,
+        2: defaultConfig,
+        3: defaultConfig,
+        4: defaultConfig,
+        5: defaultConfig,
+        6: defaultConfig,
+    }})
 
 })
