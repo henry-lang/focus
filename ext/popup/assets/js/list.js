1b636e2ef4c26a9eb13bef05f9b1c443c166fa30
Henry Langmack <Henry.langmack@gmail.com>
2021-11-06 15:00:57 -0400

    page adding and deleting

--- ext/popup/assets/js/list.js
+++ ext/popup/assets/js/list.js
@@ -5,17 +5,45 @@ let pageAdd = document.getElementById('page-add')
 let pageAddBtn = document.getElementById('page-add-btn')
 let pageAddText = document.getElementById('page-add-text')
 
-const validatePage = (value) => {}
+const validatePage = (value) => {
+    if (pages.includes(value)) return false
+    return true
+}
+
+const getPageElement = (value) => {
+    let baseNode = document.createElement('li')
+    baseNode.className = 'page'
+    baseNode.innerText = value
+
+    let deleteNode = document.createElement('button')
+    deleteNode.addEventListener('click', () => {
+        baseNode.remove()
+        pages = pages.filter((page) => page !== value)
+        console.log(pages)
+    })
+    deleteNode.className = 'page-delete'
+
+    let imageNode = document.createElement('img')
+    imageNode.src = 'assets/delete.svg'
+
+    deleteNode.appendChild(imageNode)
+    baseNode.appendChild(deleteNode)
+
+    return baseNode
+}
 
 const addPage = (value) => {
-    let node = document.createElement('li')
-    node.innerText = value
-    pageList.insertBefore(node, pageAdd)
+    let pageElement = getPageElement(value)
+    pageList.insertBefore(pageElement, pageAdd)
 
     pages.push(value)
 }
 
-const deletePage = () => {}
+const deletePage = (value) => {
+    pageList.children.map((page) => {
+        if (page.innerText == value) page.remove()
+    })
+}
 
 const addInitialPages = () => {
     // Just initial pages for now
@@ -26,7 +54,7 @@ const addInitialPages = () => {
 
 pageAddBtn.addEventListener('click', () => {
     let value = pageAddText.value
-    // if (!validatePage(value)) return
+    if (!validatePage(value)) return
     pageAddText.value = ''
     addPage(value)
 })
