331fc5e310038dec6f0ead57c15fc6b4a9c652d1
Andrew Rivera <arivera@ms-student31658.princetonk12.org>
2021-11-06 13:30:58 -0400

    closes tab on blocked sites during blocked times, single-site blocking

--- ext/background.js
+++ ext/background.js
@@ -7,11 +7,24 @@ async function get(key) {
 }
 
 chrome.storage.sync.set({timings: {
-    6: {a: '1', b: '2', c: '3'}
+    //noctural denotes the fact of the start and end times being the start and end times of rest, not work. This combats problems due to working through midnight
+    6: {nocturnal: 'false', start: '09', end: '14', blacklisted: 'https://www.reddit.com/'}
 }})
+
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     let url = tab.url
     let date = new Date()
     let blocked = await get('timings')
-    console.log(blocked.timings[6])
+    let day = date.getDay()
+    console.log(day)
+    let hour = date.getHours()
+    console.log(blocked.timings[day].start)
+    console.log(hour)
+    console.log(blocked.timings[day].end)
+    console.log(blocked.timings[day].nocturnal)
+    if (((blocked.timings[day].start < hour < blocked.timings[day].end) && (blocked.timings[day].nocturnal == "false")) || (!(blocked.timings[day].start < hour < blocked.timings[day].end) && (blocked.timings[day].nocturnal == "true"))) {
+        if (tab.url == blocked.timings[day].blacklisted) {
+            chrome.tabs.remove(tabId)
+        }
+    }
 })
