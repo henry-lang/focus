2fd3ac88e52304ed7b771958b520611f2202dda4
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2021-11-06 13:46:09 -0400

    cleanup for background.js

--- ext/background.js
+++ ext/background.js
@@ -1,30 +1,27 @@
 async function get(key) {
     return new Promise((resolve, reject) => {
         try {
-            chrome.storage.sync.get(key, (value) => {resolve(value)})
+            chrome.storage.sync.get(key, (value) => {resolve(value[key])})
         } catch (err) {reject(err)}
     })
 }
 
 chrome.storage.sync.set({timings: {
     //noctural denotes the fact of the start and end times being the start and end times of rest, not work. This combats problems due to working through midnight
-    6: {nocturnal: 'false', start: '09', end: '14', blacklisted: 'https://www.reddit.com/'}
+    6: {nocturnal: false, start: 09, end: 14, blacklisted: ['https://www.reddit.com/']}
 }})
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    let url = tab.url
     let date = new Date()
-    let blocked = await get('timings')
     let day = date.getDay()
-    console.log(day)
     let hour = date.getHours()
-    console.log(blocked.timings[day].start)
-    console.log(hour)
-    console.log(blocked.timings[day].end)
-    console.log(blocked.timings[day].nocturnal)
-    if (((blocked.timings[day].start < hour < blocked.timings[day].end) && (blocked.timings[day].nocturnal == "false")) || (!(blocked.timings[day].start < hour < blocked.timings[day].end) && (blocked.timings[day].nocturnal == "true"))) {
-        if (tab.url == blocked.timings[day].blacklisted) {
-            chrome.tabs.remove(tabId)
-        }
+    let blocked = (await get('timings'))[day]
+
+    if ((
+        (blocked.start < hour < blocked.end) && blocked.nocturnal == false) || 
+        (!(blocked.start < hour < blocked.end) && blocked.nocturnal == true)) {
+            if (blocked.blacklisted.includes(tab.url)) {
+                chrome.tabs.remove(tabId)
+            }
     }
 })
