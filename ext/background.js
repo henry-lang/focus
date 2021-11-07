888ac813201161b967e4c33b1a19dca4bc91f5f0
radicise <93229379+radicise@users.noreply.github.com>
2021-11-07 12:51:46 -0500

    code simplification, sets to default only when installed, and tab now redirects instead of closing and opening tabs, allowing the user to use the back button to return to the previous page

--- ext/background.js
+++ ext/background.js
@@ -2,14 +2,12 @@ importScripts('popup/assets/js/getFromStorage.js')
 let lastTabId = null
 let extensionDomain = new URL(chrome.runtime.getURL('popup/blocked.html')).hostname
 
-//noctural denotes the fact of the start and end times being the start and end times of rest, not work. This combats problems due to working through midnight
+//nocturnal denotes the start time being later than the end time, effectively making the focus period span through midnight
 
 const parseTime = (time) => {
-    // must be formatted in HH:MM
+    // must be formatted in 'hh:mm'
     let splitTime = time.split(':')
-    let hours = parseInt(splitTime[0])
-    let minutes = parseInt(splitTime[1])
-    return hours * 60 + minutes
+    return parseInt(splitTime[0]) * 60 + parseInt(splitTime[1])
 }
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
@@ -32,35 +30,33 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
         }
     }
     let splitBlockedName = splitTabHostname
-    if (lastTabId != tabId) {
-        if (
-            (start <= minutesSinceMidnight &&
-                minutesSinceMidnight < end &&
-                blocked.nocturnal == false) ||
-            (!(end <= minutesSinceMidnight && minutesSinceMidnight < start) &&
-                blocked.nocturnal == true)
-        ) {
-            for (let n = 0; n < blocked.blocklisted.length; n++) {
-                splitBlockedName = blocked.blocklisted[n].split('.')
-                if (splitBlockedName.length <= splitTabHostname.length) {
-                    block = true
-                    for (let o = 0; o < splitBlockedName.length; o++) {
-                        if (
-                            !(
-                                splitBlockedName[splitBlockedName.length - (o + 1)] ===
-                                splitTabHostname[splitTabHostname.length - (o + 1)]
-                            )
-                        ) {
-                            block = false
-                        }
+    if (
+        (start <= minutesSinceMidnight &&
+            minutesSinceMidnight < end &&
+            blocked.nocturnal == false) ||
+        (!(end <= minutesSinceMidnight && minutesSinceMidnight < start) &&
+            blocked.nocturnal == true)
+    ) {
+        for (let n = 0; n < blocked.blocklisted.length; n++) {
+            splitBlockedName = blocked.blocklisted[n].split('.')
+            if (splitBlockedName.length <= splitTabHostname.length) {
+                block = true
+                for (let o = 0; o < splitBlockedName.length; o++) {
+                    if (
+                        !(
+                            splitBlockedName[splitBlockedName.length - (o + 1)] ===
+                            splitTabHostname[splitTabHostname.length - (o + 1)]
+                        )
+                    ) {
+                        block = false
                     }
                 }
-                if (block == true) {
-                    chrome.tabs.remove(tabId)
-                    chrome.tabs.create({url: chrome.runtime.getURL('popup/blocked.html')})
-                    lastTabId = tabId
-                    break
-                }
+            }
+            if (block == true) {
+                console.log('11111111111111111111111111111111111111111111111111111111111111111111111')
+                chrome.tabs.update({url: chrome.runtime.getURL('popup/blocked.html')})
+                lastTabId = tabId
+                break
             }
         }
     }
@@ -70,10 +66,10 @@ chrome.runtime.onConnect.addListener((connection) => {
     connection.onMessage.addListener(async (msg) => {
         /*
             Sent in one object:
-            Type - one of the following cases.
-            Day - 0..6
-            Page? - String of the page of add/remove
-            Time? - Time to change
+            type - one of the following cases.
+            day - 0..6
+            page - String of the page of add/remove
+            time - Time to change
         */
         let data = await getFromStorage('timings')
         console.log(data)
@@ -118,35 +114,35 @@ chrome.runtime.onConnect.addListener((connection) => {
     })
 })
 
-//fix this
 chrome.runtime.onInstalled.addListener(async (details) => {
     // Ayush said foam.
-    let defaultConfig = {
-        nocturnal: false,
-        start: '09:50',
-        end: '18:30',
-        blocklisted: ['example.com'],
-        allowlisted: [
-            'https://www.youtube.com/watch?v=LqA35eLEbug',
-            'https://www.youtube.com/watch?v=jMtG9SyZfAc',
-        ],
+    if (details.reason == 'install') {
+        let defaultConfig = {
+            nocturnal: false,
+            start: '09:50',
+            end: '18:30',
+            blocklisted: ['example.com'],
+            allowlisted: [
+                'https://www.youtube.com/watch?v=LqA35eLEbug',
+                'https://www.youtube.com/watch?v=jMtG9SyZfAc',
+            ],
+        }
+        let initSettings = {
+            timings: {
+                0: defaultConfig,
+                1: defaultConfig,
+                2: defaultConfig,
+                3: defaultConfig,
+                4: defaultConfig,
+                5: defaultConfig,
+                6: defaultConfig,
+            },
+            settings: {
+                darkMode: 'off',
+                syncStorage: 'on',
+            },
+        }
+        await chrome.storage.sync.set(initSettings)
+        await chrome.storage.local.set(initSettings)
     }
-    let initSettings = {
-        timings: {
-            0: defaultConfig,
-            1: defaultConfig,
-            2: defaultConfig,
-            3: defaultConfig,
-            4: defaultConfig,
-            5: defaultConfig,
-            6: defaultConfig,
-        },
-        settings: {
-            darkMode: 'off',
-            syncStorage: 'on',
-        },
-    }
-
-    await chrome.storage.sync.set(initSettings)
-    await chrome.storage.local.set(initSettings)
 })
